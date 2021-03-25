ac9c0588c534d85374dba7df5df27e67c24e986b
Nikhil <nikhil.p@hotmail.co.uk>
2021-03-25 14:21:27 +0000

    Seperated load and create triples functions fully

--- Nikhil/create_triples.py
+++ Nikhil/create_triples.py
@@ -12,30 +12,49 @@ def load_images_by_dir(n = 0):
     image_directory = os.listdir(images_path)
     image_directory = image_directory if n == 0 or n >= len(image_directory) else image_directory[:n]
     images = {}
+    i = 0
 
     for dir in image_directory:
         path = os.path.join(images_path,dir)
         images[dir] = [cv2.imread(path + "/" + img_name) for img_name in os.listdir(path)]
+        # for img in images[dir]:
+        #     try:
+        #         assert (img.shape == (1334, 1000, 3) or img.shape == (1333, 1000, 3))
+        #     except AssertionError:
+        #         print(img.shape)
+        #         print(dir)
+
+        i+= 1
+
+        if i % 100 == 0:
+            print(i , "/ ", n)
+
 
     return images
 
-n = len(os.listdir(images_path))
+def create_triples(images):
+    triples = []
+    i = 0
+    for dir,dir_imgs in images.items():
+        idxs = np.arange(0,len(dir_imgs))
+        a_idx,p_idx = np.random.choice(idxs,size = 2, replace = False)
+        anchor, positive = dir_imgs[a_idx], dir_imgs[p_idx]
+
+        other_dirs = list(images.keys())
+        other_dirs.remove(dir)
+        negative_dir = np.random.choice(other_dirs)
+        negative = images[negative_dir][0]
+
+        triples.append(np.array([anchor,positive,negative]))
+
+    triples = np.array(triples)
+    print(triples.shape)
+    # np.save("triples.npy", triples)
+    return triples
+
+
+# n = 1500 all images
 n = 100
-images = load_images_by_dir(n)
-triples = []
-i = 0
-for dir,dir_imgs in images.items():
-    idxs = np.arange(0,len(dir_imgs))
-    a_idx,p_idx = np.random.choice(idxs,size = 2, replace = False)
-    anchor, positive = dir_imgs[a_idx], dir_imgs[p_idx]
 
-    other_dirs = list(images.keys())
-    other_dirs.remove(dir)
-    negative_dir = np.random.choice(other_dirs)
-    negative = images[negative_dir][0]
-
-    triples.append(np.array([anchor,positive,negative]))
-
-triples = np.array(triples)
-print(triples.shape)
-# np.save("triples.npy", triples)
+imgs = load_images_by_dir(n)
+triples = create_triples(imgs)
