5c369bb4ca8f4ee1cafac658860df1ca66a0866b
Nikhil <nikhil.p@hotmail.co.uk>
2021-04-05 22:58:04 +0100

    Updated Colour detection, need to play with hyper parameters for better matching

--- Nikhil/generating_diff_matrices.py
+++ Nikhil/generating_diff_matrices.py
@@ -41,7 +41,7 @@ def get_images():
 
 def get_fourier_matrix(images, image_names):
     print("Calculating fft for images...")
-    fft_images = [get_fft(i) for i in images]
+    fft_images = [get_fft(i) for i in tqdm(images)]
     n = len(images)
     fft_diff = dict([(name, {}) for name in image_names])
     matrix = np.zeros((n,n))
@@ -69,8 +69,6 @@ def get_fourier_matrix(images, image_names):
     transformed = (matrix - fft_min) / (fft_max - fft_min)
     return fft_diff, transformed
 
-
-
 def trim(im):
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
     diff = ImageChops.difference(im, bg)
@@ -79,18 +77,29 @@ def trim(im):
     if bbox:
         return im.crop(bbox)
 
-
 def get_col(image):
-    centre =  transforms.CenterCrop((30, 30))
     cropped = trim(image)
-    if cropped is None:
-        cropped = centre(image)
+    if cropped is None or min(cropped.size) < 20:
+        cropped = image
 
-    return ImageStat.Stat(cropped).median
+    median = ImageStat.Stat(cropped).median
+    if sum(median)/3 > 200:
+        cols = [tup for tup in cropped.getcolors(maxcolors=h*w) if tup[1] != (255,255,255)]
+
+        cols = sorted(cols, key=lambda x: x[0], reverse=True)
+        if cols:
+            reds = [col[0] for _, col in cols]
+            blues = [col[1] for _, col in cols]
+            greens = [col[2] for _, col in cols]
+
+            median = [np.median(cs) for cs in [reds,blues,greens]]
+            median = [int(i) for i in median]
+
+    return median
 
 def get_colour_matrix(images, image_names):
     print("Getting Colours...")
-    col_images = [get_col(i) for i in images]
+    col_images = [get_col(i) for i in tqdm(images)]
     n = len(images)
     col_diff = dict([(name, {}) for name in image_names])
     matrix = np.zeros((n, n))
@@ -126,12 +135,6 @@ def get_measures(w_matrix):
     measures = [min_w, max_w, average]
     return measures
 
-def get_error_matrix(fft_diff_matrix, col_diff_matrix):
-    col_measures = get_measures(fft_diff_matrix)
-    fft_measures = get_measures(col_diff_matrix)
-    scaler =  2 * col_measures[2] / fft_measures[2]
-    error_matrix =  scaler * fft_diff_matrix + col_diff_matrix
-    return error_matrix
 
 def convert_to_dict(matrix, names):
     n = len(names)
@@ -147,27 +150,28 @@ def convert_to_dict(matrix, names):
 
     return diff_dict
 
-
-def generate_matrices():
-    images, image_names = get_images()
+def generate_matrices(images,image_names):
     fft_diff_dict, fft_matrix = get_fourier_matrix(images, image_names)
     col_diff_dict, col_matrix = get_colour_matrix(images, image_names)
+    #Saves Transformed Matrices
+    np.save("fft_diff.npy", fft_matrix)
+    np.save("col_diff.npy", col_matrix)
 
-    np.save("fft_diff.npy", fft_diff_dict)
-    np.save("col_diff.npy", col_diff_dict)
+def generate_error_matrix():
+    fft_matrix = np.load("fft_diff.npy", allow_pickle = True).item()
+    col_matrix = np.load("col_diff.npy", allow_pickle = True).item()
 
-    error_matrix = get_error_matrix(fft_matrix, col_matrix)
+    scaler = 1
+    error_matrix = scaler * fft_matrix + col_matrix
     error_dict = convert_to_dict(error_matrix, image_names)
 
     np.save("error_diff.npy", error_dict)
-
 
 def get_closest(image_names, error_diff, idx, k):
     class_name = image_names[idx]
     row = list(error_diff[class_name].items())
     k_smallest_idx = sorted(row, key=lambda x: x[1])[1:k + 1]
     return k_smallest_idx
-
 
 def add_colour_below(images):
     joint_images = []
@@ -176,9 +180,11 @@ def add_colour_below(images):
         w,h = im.size
         mode_col = tuple(get_col(im))
         col_img = Image.new('RGB', (w, h), mode_col)
-        dst = Image.new('RGB', (w, 2 * h))
+        fft_img = Image.fromarray(get_fft(im))
+        dst = Image.new('RGB', (w, 3 * h))
         dst.paste(im, (0, 0))
         dst.paste(col_img, (0, im.height))
+        dst.paste(fft_img, (0, 2*im.height))
         joint_images.append(dst)
 
     return joint_images
@@ -192,8 +198,8 @@ def convert_names_to_images(images,image_names, closest):
 
     return closest_images
 
-def showImages(images, h = h):
-
+def showImages(images):
+    h = images[0].height
     dst = Image.new('RGB', (len(images) * w, h))
     x = 0
     y = 0
@@ -203,17 +209,46 @@ def showImages(images, h = h):
 
     dst.show()
 
-def show_example(n = 1):
-    images, image_names = get_images()
+def show_example(images,image_names,n = 1):
     error_diff = np.load("error_diff.npy", allow_pickle = True).item()
 
     for i in range(n):
         idx = random.randint(0, 1500)
+        # idx = image_names.index("15049546")
         chosen_img = images[idx]
+        print(image_names[idx])
         closest = get_closest(image_names, error_diff, idx, 10)
         closest_images = convert_names_to_images(images,image_names, closest)
         images_with_median_col = add_colour_below([chosen_img] + closest_images)
-        showImages(images_with_median_col, h = chosen_img.height * 2)
+        showImages(images_with_median_col)
 
-# generate_matrices()
-show_example(n = 5)
+
+if __name__ == '__main__':
+
+    images, image_names = get_images()
+    generate_matrices(images,image_names)
+    show_example(images,image_names,n = 10)
+
+
+    # check = ["13888239","14342457","14713205","15146351"]
+    # for i_name in check:
+    #     path = "D:\My Docs/University\Applied Data Science\Project/uob_image_set/" + i_name + "/" + i_name + "_0.jpg"
+    #     img = transform(Image.open(path))
+    #     img_with_col = add_colour_below([img])
+    #     img_with_col[0].show()
+
+    #14455761
+    #15923931
+    #13888239
+    #14342457
+    #14713205
+    #15146351
+
+    # step = 20
+    # for i in range(0, len(images), step):
+    #     start = i
+    #     batch = images[start: start + step]
+    #     batch_col = add_colour_below(batch)
+    #     showImages(batch_col)
+    #     print(image_names[start: start + step])
+    #     input()
--- Nikhil/triples_dataset.py
+++ Nikhil/triples_dataset.py
@@ -1,160 +1,34 @@
-import pickle
-
-import cv2
-import torch.utils.data as data
-from torch.utils.data import Dataset,DataLoader
-from torchvision import transforms
-
-from PIL import Image, ImageStat
-import torch
-import numpy as np
-import os
 import random
-from matplotlib import cm
+
+import numpy as np
+from PIL import Image
+from torch.utils.data import DataLoader
+from torchvision import transforms
 from torchvision.datasets import ImageFolder
-from tqdm import tqdm
+
 
 ##Generating Triples method from this article https://towardsdatascience.com/image-similarity-using-triplet-loss-3744c0f67973
 
-
-class ClothesDataset(Dataset):
-
-    def __init__(self,image_path, n , transform=None, sample_for_negatives = 100, load_data = False):
-        self.root = image_path
-        self.max_images = n
-        self.transform = transform
-        self.samples = sample_for_negatives
-
-        if load_data:
-
-            image_directory = os.listdir(self.root)
-            image_directory = image_directory if n == 0 or n >= len(image_directory) else image_directory[:n]
-            images = {}
-            i = 0
-
-            for dir in tqdm(image_directory):
-                path = os.path.join(self.root, dir)
-                images[dir] = [self.load_and_transform(path,img_name) for img_name in os.listdir(path)]
-                i += len(images[dir])
-
-            self.images = images
-            self.total_imgs = i
-            with open('images.p', 'wb') as fp:
-                pickle.dump(images, fp, protocol=pickle.HIGHEST_PROTOCOL)
-
-            print("Loaded")
-
-        else:
-            with open('images.p', 'rb') as fp:
-                images = pickle.load(fp)
-                self.total_imgs = sum(list(map(len, images.values())))
-                self.images = images
-                print("Loaded")
-
-    def __len__(self):
-        return self.total_imgs
-
-    def load_and_transform(self, path, img_name):
-        img = Image.open(path + "/" + img_name)
-
-        if self.transform:
-            img =  self.transform(img)
-
-        return np.array(img)
-
-    def __getitem__(self, idx):
-        idx = np.random.randint(0, len(self.images.keys()))
-        chosen_dir,dir_imgs = list(self.images.items())[idx]
-
-        idxs = np.arange(1, len(dir_imgs))
-        a_idx, p_idx = 0, np.random.choice(idxs)
-        anchor, positive = dir_imgs[a_idx], dir_imgs[p_idx]
-
-        other_dirs = list(self.images.keys())
-        other_dirs.remove(chosen_dir)
-        negative = self.get_close_negative(anchor, other_dirs)
-
-        triple = [anchor,positive,negative]
-
-        return tuple(triple)
-
-    def get_close_negative(self, anchor, other_dirs):
-        w, h, _ = anchor.shape
-        anchor = Image.fromarray(anchor)
-
-        negative_images_dir = random.sample(other_dirs, self.samples)
-        smallest_diff = None
-
-        for n_dir in tqdm(negative_images_dir):
-            neg_img = Image.fromarray(self.images[n_dir][0])
-
-            neg_diff = 1/(w * h) * np.sum(abs(anchor - neg_img))
-
-            if smallest_diff == None or smallest_diff > neg_diff:
-                smallest_diff = neg_diff
-                closest_negative = neg_img
-
-        return closest_negative
-
 class ClothesFolder(ImageFolder):
     
-    def __init__(self,image_path, n , transform=None, load_data = False):
+    def __init__(self,image_path, n , transform=None):
         super(ClothesFolder, self).__init__(root=image_path, transform = transform)
         # super().__init__(root=image_path, transform = transform,sample_for_negatives,load_data)
     
         self.max_images = n
-        # self.samples = sample_for_negatives
-        self.col_diff = np.load('col_diff.npy',allow_pickle=True).item()
-        self.fft_diff = np.load('fft_diff.npy',allow_pickle=True).item()
+
         self.error_diff = np.load('error_diff.npy',allow_pickle=True).item()
-        # print(self.fft_diff.keys())
-        # print(self.classes)
 
-        # if load_data:
-        #     # Create a dictionary of lists for each class for reverse lookup
-        #     # to generate triplets 
-        #     images= {}
-        #     for c in self.classes:
-        #         ci = self.class_to_idx[c]
-        #         images[ci] = []
-        #     # append each file in the approach dictionary element list
-        #     for s in self.samples:
-        #         images[s[1]].append(s[0])
-        #     # images[s[1]] = [self.load_and_transform(path,img_name) for img_name in os.listdir(path)]
-        #     # keep track of the sizes for random sampling
-        #     # self.total_imgs = {}
-        #     # for c in self.classes:
-        #     #     ci = self.class_to_idx[c]
-        #     #     self.total_imgs[ci] = len(images[ci])
-        #     self.total_imgs = sum(list(map(len, images.values())))
-        #     self.images = images
-        #     with open('images.p', 'wb') as fp:
-        #         pickle.dump(images, fp, protocol=pickle.HIGHEST_PROTOCOL)
         self.images = {}
-        if load_data:
-            for dir in self.classes:
-                class_index = self.class_to_idx[dir]
-                self.images[class_index] = []
-            for s in self.samples:
-                self.images[s[1]].append(s[0])
-            self.classdictsize = {}
-            for c in self.classes:
-                ci = self.class_to_idx[c]
-                self.classdictsize[ci] = len(self.images[ci])    
-
-            # print(self.samples[0])
-        else:
-            with open('images.p', 'rb') as fp:
-                self.images = pickle.load(fp)
-                self.total_imgs = sum(list(map(len, self.images.values())))
-                # self.total_imgs = {}
-                # for c in self.classes:
-                #     ci = self.class_to_idx[c]
-                #     self.total_imgs[ci] = len(images[ci])
-                print("Loaded")     
-    
-    # def __len__(self):
-    #     return self.classdictsize
+        for dir in self.classes:
+            class_index = self.class_to_idx[dir]
+            self.images[class_index] = []
+        for s in self.samples:
+            self.images[s[1]].append(s[0])
+        self.classdictsize = {}
+        for c in self.classes:
+            ci = self.class_to_idx[c]
+            self.classdictsize[ci] = len(self.images[ci])
 
     def load_and_transform(self, path, img_name):
         img = Image.open(path + "/" + img_name)
@@ -198,7 +72,7 @@ if __name__ == '__main__':
 
     transform = transforms.Resize((1333,1000))
     # dataset = ClothesDataset(images_path, 500, transform = transform, sample_for_negatives= 99, load_data=False)
-    dataset = ClothesFolder(images_path, 500, transform = transform, load_data=True)
+    dataset = ClothesFolder(images_path, 500, transform = transform)
     dataloader = DataLoader(dataset, batch_size = 5, shuffle=True)
     i = random.randint(0,1500)
     test = dataset[i]
